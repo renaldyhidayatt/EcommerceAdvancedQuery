e1290fd3150f286549ca4d2ab42c9c7cf6e51c2a
renaldyhidayatt <renaldyhidayatt@gmail.com>
2023-01-27 17:14:17 +0700

    Adding

---
+++ src/main/java/com/sanedge/ecommercequery/EcommercequeryApplication.java
@@ -0,0 +1,13 @@
+package com.sanedge.ecommercequery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercequeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercequeryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanedge/ecommercequery/EcommercequeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanedge.ecommercequery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercequeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
